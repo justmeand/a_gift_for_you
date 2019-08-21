c257609478d064ee949ba01287239c0138975c35
ninedyseven <ninedyseven@gmail.com>
2019-08-22 00:12:49 +0700

    update

--- js/script.js
+++ js/script.js
@@ -7,19 +7,31 @@ function preload(){
 function setup(){
     song.play();
 
-    $("#wait_text").fadeIn("slow", function() {
+    $("#wait_text").fadeOut("slow", function() {
         $(this).addClass("hidden");
     });
-    $("#loading").fadeIn("slow", function() {
+    $("#loading").fadeOut("slow", function() {
         $(this).addClass("hidden");
     });
 
-    $("#main_box").fadeOut("slow", function() {
+    $("#main_box").fadeIn("slow", function() {
         $(this).removeClass("hidden");
     });
 
-    $("#guide_text").fadeOut("slow", function() {
+    $("#guide_text").fadeIn("slow", function() {
         $(this).removeClass("hidden");
+    });
+
+    $("#main_box").hover(function(){
+        $("#guide_text").fadeOut( 1000 );
+        $("#guide_text").addClass("hidden");
+        $("#open_box_text").fadeIn( 1000 );
+        $("#open_box_text").removeClass("hidden");
+    }, function(){
+        $("#guide_text").fadeIn( 1000 );
+        $("#guide_text").removeClass("hidden");
+        $("#open_box_text").fadeOut( 1000 );
+        $("#open_box_text").addClass("hidden");
     });
     
     
