7b0aff9bcc28581f0c5da6f49f8e43ed8210f1ca
ninedyseven <ninedyseven@gmail.com>
2019-08-22 00:49:27 +0700

    update yt

--- js/script.js
+++ js/script.js
@@ -1,27 +1,22 @@
-let song;
-function preload(){
-    song = loadSound("./music/bg.mp3");
-    document.getElementById("p5_loading").remove();
-}
-
-function setup(){
-    song.play();
-
-    $("#wait_text").fadeOut("slow", function() {
-        $(this).addClass("hidden");
-    });
-    $("#loading").fadeOut("slow", function() {
-        $(this).addClass("hidden");
-    });
-
-    $("#main_box").fadeIn("slow", function() {
-        $(this).removeClass("hidden");
-    });
-
-    $("#guide_text").fadeIn("slow", function() {
-        $(this).removeClass("hidden");
-    });
-
+setTimeout(function(){
+    $(document).ready(function(){
+        $("#wait_text").fadeOut("slow", function() {
+            $(this).addClass("hidden");
+        });
+        $("#loading").fadeOut("slow", function() {
+            $(this).addClass("hidden");
+        });
+        
+        $("#main_box").fadeIn("slow", function() {
+            $(this).removeClass("hidden");
+        });
+        
+        $("#guide_text").fadeIn("slow", function() {
+            $(this).removeClass("hidden");
+        });
+    })
+    
+    
     $("#main_box").hover(function(){
         $("#guide_text").fadeOut( 1000 );
         $("#guide_text").addClass("hidden");
@@ -33,10 +28,4 @@ function setup(){
         $("#open_box_text").fadeOut( 1000 );
         $("#open_box_text").addClass("hidden");
     });
-    
-    
-    document.getElementById("defaultCanvas0").remove();
-}
-
-
-
+}, 2000);
